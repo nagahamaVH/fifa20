60abac67560ae8d2090bb9839ae3e85ee5d96eff
nagahamavh <victorh.nagahama@gmail.com>
2019-11-27 15:58:51 -0300

    adicao de regras

--- R/lp.R
+++ R/lp.R
@@ -32,12 +32,24 @@ model <- MILPModel() %>%
   # Função objetiva: maximizar a soma do escore do time
   set_objective(sum_expr(colwise(
     getFromMatrix(playersStat, i, j) * dummyPosition[i, j], i = 1:n, 
-    j = 1:m))) %>%
+    j = 1:m)), sense = 'max') %>%
   
   # R: A quantidade total de jogadores na posição deve ser igual a quantidade 
-  # especificada na formação escolhida 
-  add_constraint(sum_expr(dummyPosition[i, j], i = 1:n) - 
-                   getFromMatrix(formations, i, j) < 1, i = 1:p, j = 1:m)
+  # especificada na formação escolhida
+  # add_constraint(sum_expr(dummyPosition[i, j], i = 1:n) -
+  #                  getFromMatrix(formations, i, j) * C[k] == 0, i = 1:p, j = 1:m,
+  #                k = 1:p) %>%
+  
+  # R: O jogador só pode ser designado para uma única posição
+  add_constraint(sum_expr(dummyPosition[i, j], j = 1:m) <= 1, i = 1:n) %>%
+  
+  # R: A formação tática deve ser única
+  add_constraint(sum_expr(C[k], k = 1:p) == 1)
 
 result <- solve_model(model, with_ROI(solver = "symphony", 
                                       presolve = T, time_limit = 5 * 60))
+
+# Resultado do modelo
+result %>%
+  get_solution(dummyPosition[i, j]) %>%
+  filter(value == 1)
